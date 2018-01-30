5b6d76dc0002bf9e1662b07fdcd0953907c99088
Brandon Stallo <brandon.stallo@gmail.com>
2018-01-30 11:35:45 -0600

    asf
    rea

---
+++ content/script/controller/handicap-service.js
@@ -0,0 +1,20 @@
+﻿(function() {
+    'use strict';
+    angular
+        .module('handicapApp')
+        .factory('HandicapService', HandicapService);
+
+    HandicapService.$inject = ['$firebaseObject'];
+
+    function HandicapService($firebaseObject) {
+        var ref = firebase.database().ref();
+
+        return {
+            getTeams: getTeams,
+        }
+
+        function getTeams() {
+
+        }
+    }
+})();
---
+++ content/script/controller/teams-controller.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('handicapApp')
+        .controller('TeamController', TeamController);
+
+    TeamController.$inject = ['$firebaseArray'];
+
+    function TeamController(
+        $firebaseArray) {
+        var vm = this;
+        var ref = firebase.database().ref().child('teams');
+        vm.message = 'Calculate Handicap';
+        vm.teams = $firebaseArray(ref);
+        vm.isTeamSelected = false;
+        vm.myFunc = myFunc;
+        vm.populateTeamInfo = populateTeamInfo;
+
+        activate();
+
+        function activate() {
+            myFunc();
+            //populateTeamInfo()
+
+        };
+
+        function myFunc() {
+            console.log('hello');
+        };
+
+        function populateTeamInfo(team) {
+            vm.isTeamSelected = true;
+            vm.selectedTeam = team;
+        };
+
+
+    }
+}());
