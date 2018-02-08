a824b108b8bc35f75b13caca6161c39c1d2788e9
Brandon Stallo <brandon.stallo@gmail.com>
2018-02-08 12:05:38 -0600

    got scores setup
    got scores setup and submitting

--- content/script/controller/team-service.js
+++ content/script/controller/team-service.js
@@ -4,39 +4,47 @@
         .module('handicapApp')
         .factory('TeamService', TeamService);
 
-    HandicapService.$inject = ['$firebaseObject'];
+    TeamService.$inject = ['$http', '$q','$sce', '$firebaseObject', '$rootScope'];
 
-    function TeamService($firebaseObject) {
-        //var ref = firebase.database().ref();
-        var vm = this;
-        var ref = firebase.database().ref().child('teams');
-        vm.teams = $firebaseArray(ref);
-
+    function TeamService($http, $q, $sce, $firebaseObject, $rootScope) {
 
         return {
-            getTeams: getTeams,
+            //getTeams: getTeams,
             createTeam: createTeam,
         }
 
-        function getTeams() {
-
-        }
+        //var ref = firebase.database().ref();
 
         function createTeam() {
-            var numberOfTeams = vm.teams.length;
+     
 
-            var team = {
-                id: numberOfTeams + 1,
-                name: vm.name,
-                player1: vm.player1Name,
-                player2: vm.player2Name,
+            //var ref = firebase.database().ref().child('teams');
 
-            };
+            //var url = $sce.trustAsResourceUrl('https://smgc-1487256758030.firebaseio.com')
+            //var requestUrl = url
+            
+            ////return $http.get(requestUrl)
+            //    return ref
 
-            vm.teams.$add(team);
-            //vm.teams.push(team);
+            //    .then(function(response) {
+            //        var response = 999;
 
-            return team
+            //    }).catch(function(response) {
+            //        var exception = {
+            //            message: 'Sorry, we are unable to retrieve your pending documents.',
+            //            type: 'error',
+            //        };
+            //        return $q.reject(exception);
+            //    });
+
+
+
         }
+
+
+
+        //function getTeams() {
+
+        //}
     }
 })();
--- content/script/controller/teams-controller.js
+++ content/script/controller/teams-controller.js
@@ -5,47 +5,89 @@
         .module('handicapApp')
         .controller('TeamController', TeamController);
 
-    TeamController.$inject = ['$firebaseArray'];
+    TeamController.$inject = ['TeamService', '$firebaseArray', '$firebaseObject'];
 
     function TeamController(
-        $firebaseArray) {
+        TeamService,
+        $firebaseArray,
+        $firebaseObject) {
         var vm = this;
-        var ref = firebase.database().ref().child('teams');
-        vm.message = 'Calculate Handicap';
-        vm.teams = $firebaseArray(ref);
+        var ref = firebase.database().ref();
+        vm.teams = $firebaseArray(ref.child('teams'));
+        vm.scores = $firebaseArray(ref.child('scores'));
         vm.isTeamSelected = false;
+        vm.getTeams = getTeams;
         vm.createTeam = createTeam;
+        vm.removeTeam = removeTeam;
         vm.populateTeamInfo = populateTeamInfo;
+        vm.enterNewScore = enterNewScore;
+        vm.submitScore = submitScore;
 
         activate();
 
+ 
+
         function activate() {
-            populateTeamInfo()
-            console.log($firebaseArray(ref));
+         
+        }
+
+
+        function getTeams() {
+            console.log('get teams');
 
         }
 
         function createTeam() {
+
             var numberOfTeams = vm.teams.length;
 
             var team = {
                 id: numberOfTeams + 1,
+                handicap: 0,
+                scores: [],
                 name: vm.name,
                 player1: vm.player1Name,
-                player2: vm.player2Name,
-
+                player2: vm.player2Name
             };
 
-            vm.teams.$add(team);
-            //vm.teams.push(team);
+            vm.teams.$add(team).then(function(ref) {
+                var id = ref;
+                console.log("added record with id " + id);
+                console.log(vm.teams.$indexFor(id)); // returns location in the array
+            });
 
-            return team
+
+            //TeamService.createTeam()
+            //    .then(function(response) {
+            //        console.log(response);
+            //    }).catch(function(exception) {
+
+            //    }).finally(function() {
+
+            //    });
+        }
+
+        function removeTeam(team) {
+            var deletedTeam = vm.teams.indexOf(team);
+            vm.teams.$remove(team);
         }
 
         function populateTeamInfo(team) {
-            console.log('pop team');
-            //vm.isTeamSelected = true;
-            //vm.selectedTeam = team;
+            vm.isTeamSelected = true;
+            vm.selectedTeam = team;
+
+        function enterNewScore() {
+            vm.enterNewScore = true;
+        }
+
+        function submitScore(score) {
+
+            var teamID = vm.selectedTeam.$id
+            var list = $firebaseArray(ref.child('teams/' + teamID + '/scores'));
+            var newScore = parseInt(score);
+
+            list.$add(newScore);
+
         }
     }
 }());
